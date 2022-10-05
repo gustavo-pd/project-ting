9f164c3a037e4a188fd4e748952d88140138d1d5
GustavoPD <gudalmoro@gmail.com>
2022-10-05 00:28:39 -0300

    File management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+import os
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not os.path.exists(path_file):
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+    if ".txt" not in path_file:
+        return sys.stderr.write("Formato inválido\n")
+
+    with open(path_file) as file:
+        split_file = file.read().splitlines()
+        return split_file
